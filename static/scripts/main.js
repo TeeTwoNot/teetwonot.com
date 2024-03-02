273107edb5829acf4ffcf5dd51cee5754b4ac857
TeeTwoNot <85189523+TeeTwoNot@users.noreply.github.com>
2024-03-02 03:48:40 +0200

    Message for unsuitable devices

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -21,3 +21,7 @@ function topFunc() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+if (window.innerWidth < 840) {
+  alert('This website is not properly formatted for mobile devices. If possible, visit from a Desktop.');
+}
